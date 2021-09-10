291fd56e85ca82b2f78671ca087e9705b79ee459
Shaidar Haran <leo.yague@gmail.com>
2021-09-10 18:01:56 +0200

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Header from './Components/Header';
+
 import 'normalize.css';
 import './GlobalStyle.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/search">Search</Route>
         <Route path="/">Home</Route>
