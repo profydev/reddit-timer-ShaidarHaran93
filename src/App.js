9c9a501cda4e3a15552549f5d425ef37d1045613
Shaidar Haran <leo.yague@gmail.com>
2021-09-10 17:34:34 +0200

    Added router with Home and Search routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Route, Router, Switch } from 'react-router';
+
 import Normalize from 'normalize.css';
 import GlobalStyle from './GlobalStyle.css';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Normalize />
       <GlobalStyle />
+      <Switch>
+        <Route path="/search">Search</Route>
+        <Route path="/">Home</Route>
+      </Switch>
       App Placeholder
-    </div>
+    </Router>
   );
 }
 
