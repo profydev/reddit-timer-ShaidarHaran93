a3993eee51d448914cd749d4a18d6000f472afaf
Shaidar Haran <leo.yague@gmail.com>
2021-09-10 17:30:50 +0200

    Added Global Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Normalize from 'normalize.css';
+import GlobalStyle from './GlobalStyle.css';
 
 function App() {
   return (
     <div>
       <Normalize />
+      <GlobalStyle />
       App Placeholder
     </div>
   );
