e6c7908870834547bc52ba1c131e0bb86bfb535a
ShaidarHaran93 <58345368+ShaidarHaran93@users.noreply.github.com>
2021-09-13 13:16:18 +0200

    Create app skeleton (#1)
    * Installed Bitter and Montserrat fonts
    
    * Added normalize.css
    
    * Added Global Styles
    
    * Added router with Home and Search routes
    
    * Fixed router package and css imports
    
    * Switched fonts from Google Fonts to local
    
    * Added styled-components and refactor accordingly
    
    * Changed app title, logos and meta description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route exact path="/search">Search</Route>
+          <Route path="/">Home</Route>
+          <Route>404 - Not Found</Route>
+        </Switch>
+        App Placeholder
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
